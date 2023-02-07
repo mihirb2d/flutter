ad33a6d33b5782619f0597ccf562272fa019bf41
mihirchauhan2000 <93507408+mihirchauhan2000@users.noreply.github.com>
2023-02-07 10:50:43 +0530

    update

---
+++ dart_programming/Assignment/p1_personalinfo.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  String name = "Mihir B Chauhan";
+  String dob = "14 Apr 2000";
+  int age = 22;
+  String address = "Old City, Gheekanta, Bhadra, Ahmedabad, Gujarat 380001";
+
+  print(name);
+  print(dob);
+  print(age);
+  print(address);
+}
